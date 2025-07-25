738c76db8848d234c79f896f301fe2813379cc84
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2025-07-25 12:00:34 -0400

    messing with slider when on/off

--- ReactTypescriptPlayground/src/Components/ToggleButton.tsx
+++ ReactTypescriptPlayground/src/Components/ToggleButton.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-import Style from './toggleButton.module.scss'
+import React, { useState, useRef, useEffect } from "react";
+import classNames from "classnames";
+import Style from "./toggleButton.module.scss";
 
 interface ToggleSliderProps {
   label: string;
@@ -8,16 +8,52 @@ interface ToggleSliderProps {
 
 const ToggleSlider: React.FC<ToggleSliderProps> = ({ label }) => {
   const [isOn, setIsOn] = useState(false);
+  const intervalRef = useRef<number | null>(null);
 
   const toggle = () => {
     setIsOn(!isOn);
+    if (isOn === false) {
+      // Clear any existing interval first
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+      // Start new interval and store the ID
+      intervalRef.current = setInterval(() => {
+        console.log("Toggle is on");
+      }, 1000);
+    } else if (isOn === true) {
+      console.log("Toggle is off");
+      // Clear the interval when turning off
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    }
   };
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
 
   return (
     <div className={Style.toggleContainer}>
       <label className={Style.toggleLabel}>{label}</label>
-      <div className={classNames(Style.toggleSlider, { [Style.on]: isOn, [Style.off]: !isOn })} onClick={toggle}>
-          <div className={classNames(Style.onOrOff, { [Style.off]: !isOn })}> { isOn ? 'yes' : 'no'} </div>
+      <div
+        className={classNames(Style.toggleSlider, {
+          [Style.on]: isOn,
+          [Style.off]: !isOn,
+        })}
+        onClick={toggle}
+      >
+        <div
+          className={classNames(Style.onOrOff, { [Style.off]: isOn === false })}
+        >
+          {isOn ? "yes" : "no"}{" "}
+        </div>
         <div className={Style.toggleThumb}></div>
       </div>
     </div>
--- ReactTypescriptPlayground/src/Pages/Test.tsx
+++ ReactTypescriptPlayground/src/Pages/Test.tsx
@@ -1,21 +1,30 @@
 import ButtonToPath from "../Components/ButtonToPath.tsx";
-import Card from "../Components/Card.tsx"
-import style from "../Components/style.module.scss"
-import '../Styling/test-page.css'
-import ToggleSlider from "../Components/ToggleButton.tsx"
+import Card from "../Components/Card.tsx";
+import style from "../Components/style.module.scss";
+import "../Styling/test-page.css";
+import ToggleSlider from "../Components/ToggleButton.tsx";
 
 export default function Test() {
-    return(
-        <>
-            <nav className={'test-nav'}>
-                <ButtonToPath customStyle={style.testStyle} path={'/'} text={"GO HOME"}/>
-                <ButtonToPath customStyle='test-style' path={'/'} text={"GO HOME"}/>
-                <ButtonToPath customStyle='test-style' path={'/'} text={"GO HOME"}/>
-                <ButtonToPath customStyle='test-style' path={'/'} text={"GO HOME"}/>
-                <ButtonToPath customStyle={style.testStyle}  path={'/'} text={"GO HOME"}/>
-            </nav>
-            <Card/>
-            <ToggleSlider label="test"/>
-        </>
-    )
+  return (
+    <>
+      <nav className={"test-nav"}>
+        <ButtonToPath
+          customStyle={style.testStyle}
+          path={"/"}
+          text={"GO HOME"}
+        />
+        <ButtonToPath customStyle="test-style" path={"/"} text={"GO HOME"} />
+        <ButtonToPath customStyle="test-style" path={"/"} text={"GO HOME"} />
+        <ButtonToPath customStyle="test-style" path={"/"} text={"GO HOME"} />
+        <ButtonToPath
+          customStyle={style.testStyle}
+          path={"/"}
+          text={"GO HOME"}
+        />
+      </nav>
+      <Card />
+      <ToggleSlider label="Do you like ice cream?" />
+      <ToggleSlider label="Are you eligible to apply?" />
+    </>
+  );
 }
