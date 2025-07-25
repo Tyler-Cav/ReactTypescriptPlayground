d4edaa7e16a3e5e39bd86abf1a47c6254f88e2de
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2025-07-25 11:25:51 -0400

    messing with component onclick prop

--- ReactTypescriptPlayground/src/Components/Button.tsx
+++ ReactTypescriptPlayground/src/Components/Button.tsx
@@ -1,26 +1,26 @@
 import * as React from "react";
 
 const style: object = {
-    fontSize: "2.5vw",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "fit-content",
-    width: "fit-content",
-    color: "rgb(0, 0, 0, 1)",
-    backgroundColor: "rgb(0, 0, 0, .2)"
+  fontSize: "2.5vw",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "fit-content",
+  width: "fit-content",
+  color: "rgb(0, 0, 0, 1)",
+  backgroundColor: "rgb(0, 0, 0, .2)",
 };
 
 interface RegularButtonProps {
-    text?: string;
-    customStyle?: string;
-    onClick: () => React.MouseEvent<HTMLButtonElement>
+  id: string;
+  text?: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
-export default function Button({text, customStyle, onClick }: RegularButtonProps) {
-    return (
-        <button style={style} onClick={onClick} value={text}>
-            {text}
-        </button>
-    );
+export default function Button({ id, text, onClick }: RegularButtonProps) {
+  return (
+    <button id={id} style={style} onClick={onClick} value={text}>
+      {text}
+    </button>
+  );
 }
--- ReactTypescriptPlayground/src/Components/Card.tsx
+++ ReactTypescriptPlayground/src/Components/Card.tsx
@@ -1,33 +1,38 @@
-import Button from "../Components/Button.tsx"
+import React from "react";
+import Button from "../Components/Button.tsx";
 
-let Style: object  = {
-    display: "flex",
-    height: "auto",
-    width: "90px",
-    padding: "15px",
-    margin: "10px",
-    border: "2px solid black",
-    flexDirection: "column",
-    justifyContent: "start",
+let Style: object = {
+  display: "flex",
+  height: "auto",
+  width: "50%",
+  padding: "15px",
+  margin: "10px",
+  border: "2px solid black",
+  flexDirection: "column",
+  justifyContent: "start",
+  alignItems: "start",
+  gap: "100px",
 };
 
-const testClick = (e:React.MouseEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget.value)
-    return (
-        <div>hello</div>
-    )
+const testClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  console.log(e.currentTarget.textContent);
+
+  // Create a new div element
+  const newDiv = document.createElement("div");
+  const thisCardEl: HTMLElement | null = document.getElementById("hello");
+  newDiv.textContent = "New div added!";
+  newDiv.style.cssText =
+    "background-color: lightblue; padding: 5px; margin: 2px; border-radius: 3px;";
+
+  // Append the div to the button
+  thisCardEl.appendChild(newDiv);
 };
 
 export default function Card() {
-    return (
-        <div style={Style}>
-            <div>
-                ardas;dlfjk
-                a;lsdkfj
-                a;ldskjf
-                a;ldfkj
-            </div>
-            <Button text="asf" onClick={testClick}/>
-        </div>
-    );
+  return (
+    <div style={Style}>
+      <div id="hello">This is my card.</div>
+      <Button id="asd" text="asd" onClick={testClick} />
+    </div>
+  );
 }
